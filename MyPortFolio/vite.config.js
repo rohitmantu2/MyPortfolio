c432e07f58e72e133dd3fb60e4933b634868e82a
Rohit kumar <53420093+rohitmantu2@users.noreply.github.com>
2026-03-20 17:52:37 +0530

    deployment changes

--- MyPortFolio/vite.config.js
+++ MyPortFolio/vite.config.js
@@ -3,9 +3,12 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     babel({ presets: [reactCompilerPreset()] })
+//   ],
+// })
+export default {
+  base: "MyPortfolio",
+};
